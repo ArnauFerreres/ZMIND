73d3995233aeedfe74ca1356740bcbe363ae2acb
CryBabyGetCake <talismant03@gmail.com>
2023-02-13 12:53:58 +0100

    Volumen Efectos De Sonido

--- ZMIND/Assets/Scripts/Volumen/VolumenController.cs
+++ ZMIND/Assets/Scripts/Volumen/VolumenController.cs
@@ -5,9 +5,18 @@ using UnityEngine.Audio;
 public class VolumenController : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    
+    [SerializeField] public AudioSource soundPlayer;
 
     public void ControlVolumen(float sliderAudio)
     {
         audioMixer.SetFloat("Volumen", Mathf.Log10(sliderAudio)*20);
     }
+
+    public void playThisSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+
+
 }
