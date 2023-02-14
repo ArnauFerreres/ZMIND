be79ca5e4d3f125ed0bd9e7561ce1c7abe688363
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2023-02-14 14:04:21 +0100

    Blood pruebas

--- ZMIND/Assets/Scripts/BoneController.cs
+++ ZMIND/Assets/Scripts/BoneController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BoneController : MonoBehaviour, iTakeItem
 {
+    public GameObject blood;
     void iTakeItem.TakeItem()
     {
         GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().UpdateTotalCoins();
+        //Instantiate(blood).transform.position;
         Destroy(gameObject);
     }
 }
