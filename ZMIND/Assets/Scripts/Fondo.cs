fc75c0eb020297189b3ba7689cdf272cfb5ff244
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2023-02-21 13:14:09 +0100

    Fondo no se mueve pero se intenta

--- ZMIND/Assets/Scripts/Fondo.cs
+++ ZMIND/Assets/Scripts/Fondo.cs
@@ -1,9 +1,13 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
+
 
 public class Fondo : MonoBehaviour
 {
+    public float Speed = 1.5f;
+    Vector2 FondoPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class Fondo : MonoBehaviour
     void Update()
     {
         
+    }
+    void MoveFondo()
+    {
+        FondoPos = new Vector2(Time.time * Speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset= FondoPos;
     }
 }
