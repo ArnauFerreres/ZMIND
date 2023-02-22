e8312798a842bd8133ff76510becf9437701db6e
PauSori <pausorianoj@gmail.com>
2023-02-22 16:54:25 +0100

    Ajustar Volumen (corregido)
    volumen y slider se guarda en todas las escenas

--- ZMIND/Assets/Scripts/Volumen/VolumenController.cs
+++ ZMIND/Assets/Scripts/Volumen/VolumenController.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 public class VolumenController : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    
+
     [SerializeField] public AudioSource soundPlayer;
+
+    [SerializeField] public Slider sliderMusic;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Volumen"))
+        {
+            sliderMusic.value = PlayerPrefs.GetFloat("Volumen");
+        }
+        else 
+        { 
+            sliderMusic.value = 0.5f; 
+        }
+    }
 
     public void ControlVolumen(float sliderAudio)
     {
-        audioMixer.SetFloat("Volumen", Mathf.Log10(sliderAudio)*20);
+        audioMixer.SetFloat("Volumen", Mathf.Log10(sliderAudio) * 20);
+        PlayerPrefs.SetFloat("Volumen", sliderAudio);
     }
 
     public void playThisSoundEffect()
